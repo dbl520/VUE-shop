e1838d46d6098236bae4f69672dd0f7088bbcf2c
zhuzhudengbaoling <2239146847@qq.com>
2018-01-13 23:36:31 +0800

    6
    6

--- vue_mint_app/src/pages/my.vue
+++ vue_mint_app/src/pages/my.vue
@@ -32,10 +32,10 @@
         </div>
       </div>
         <!-- 地址 -->
- <mt-picker :slots="addressSlots" class="picker" @change="onAddressChange" :visible-item-count="5"  v-if="showhide==0"></mt-picker >
-
-<mt-picker :slots="streetSlots" ref="picker" class="picker" @change="onStreetChange" :visible-item-count="5" v-if="showhide==0"></mt-picker >
-<div v-if="showhide==0">地址:{{ addressProvince }} {{ addressCity }} {{addressXian}} {{addressStreet}}</div> 
+        <mt-popup   v-model="popupVisible" position="bottom" >
+    <mt-picker :slots="myAddressSlots" @change="onMyAddressChange"></mt-picker>
+</mt-popup>
+  <p>地址3级联动：{{myAddressProvince}} {{myAddressCity}} {{myAddresscounty}}</p>
     </div>
     <!-- 底部组件 -->
     <footer-bar class="footer"></footer-bar>
@@ -44,8 +44,13 @@
 <script>
 // 引入组件
 import Footer from "../components/FooterBar.vue";
-import { MessageBox } from "mint-ui";
-import s from "../../static/data/address4.json";
+// 弹出框
+import Vue from "vue";
+import { Popup } from "mint-ui";
+Vue.component(Popup.name, Popup);
+// 地址三级联动json数据
+import { Toast, Indicator, MessageBox } from "mint-ui";
+import myaddress from "../../static/data/address3.json";
 // 引入地址picker
 export default {
   // 开始
@@ -55,13 +60,11 @@ export default {
   name: "my",
   data() {
     return {
-      showhide:1,//控制地址是否显示
-      companyName: "",
-      addressSlots: [
+      myAddressSlots: [
         {
           flex: 1,
           defaultIndex: 1,
-          values: Object.keys(s),
+          values: Object.keys(myaddress), //省份数组
           className: "slot1",
           textAlign: "center"
         },
@@ -88,39 +91,22 @@ export default {
           textAlign: "center"
         }
       ],
-      streetSlots: [
-        {
-          flex: 1,
-          values: [],
-          className: "slot1",
-          textAlign: "center"
-        }
-      ],
-      addressProvince: "省",
-      addressCity: "市",
-      addressXian: "区",
-      addressStreet: "街道"
+      myAddressProvince: "省",
+      myAddressCity: "市",
+      myAddresscounty: "区/县"
     };
   },
   methods: {
-    // 地址事件
-    onAddressChange(picker, values) {
-      console.log("测试",picker, values)
-      let sheng = Object.keys(s);
-      let shi = Object.keys(s[values[0]]);
-      let index = shi.indexOf(values[1]);
-      let xian = s[values[0]][shi[index]];
-
-      this.xianObj = xian;
-      picker.setSlotValues(1, shi);
-      this.addressProvince = values[0];
-      this.addressCity = values[1];
-      this.addressXian = values[2];
-      picker.setSlotValues(2, Object.keys(xian));
-    },
-    onStreetChange(picker, values) {
-      this.addressStreet = values[0];
-       console.log("测试2",picker, values)
+    //获取地区列表
+    onMyAddressChange(picker, values) {
+      if (myaddress[values[0]]) {
+        //这个判断类似于v-if的效果（可以不加，但是vue会报错，很不爽）
+        picker.setSlotValues(1, Object.keys(myaddress[values[0]])); // Object.keys()会返回一个数组，当前省的数组
+        picker.setSlotValues(2, myaddress[values[0]][values[1]]); // 区/县数据就是一个数组
+        this.myAddressProvince = values[0];
+        this.myAddressCity = values[1];
+        this.myAddresscounty = values[2];
+      }
     },
     order: function() {
       this.$router.push({ path: "all_order" });
@@ -148,31 +134,24 @@ export default {
     //地址管理
     dizhi: function() {
       var _this = this;
-      console.log(_this.showhide)
-      _this.showhide==0;
-      if(_this.showhide==0){
-        _this.showhide=1
-      }else{
-         console.log(_this.showhide)
-         _this.showhide=0
+      console.log(_this.showhide);
+      _this.showhide == 0;
+      if (_this.showhide == 0) {
+        _this.showhide = 1;
+      } else {
+        console.log(_this.showhide);
+        _this.showhide = 0;
       }
     }
   },
-  watch: {
-    addressXian: {
-      handler(val, oval) {
-        let street = this.xianObj[this.addressXian];
-        this.streetSlots[0].values = street;
-      }
-    }
-  },
+  watch: {},
   created() {},
   mounted() {
     this.$nextTick(() => {
-      setTimeout(() => {
-        //这个是一个初始化默认值的一个技巧
-        this.addressSlots[0].defaultIndex = 0;
-      }, 100);
+      //vue里面全部加载好了再执行的函数  （类似于setTimeout）
+      this.myAddressSlots[0].defaultIndex = 0;
+      // 这里的值需要和 data里面 defaultIndex 的值不一样才能够初始化
+      //因为我没有看过源码（我猜测是因为数据没有改变，不会触发更新）
     });
   }
 };
