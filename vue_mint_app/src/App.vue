e6bd62bb850af03ee722570b85f76a8472c45c49
zhuzhudengbaoling <2239146847@qq.com>
2018-06-15 14:48:35 +0800

    10
    10

--- vue_mint_app/src/App.vue
+++ vue_mint_app/src/App.vue
@@ -1,40 +1,56 @@
 <template>
   <div id="app">
-      <transition name="slide-fade">
+    <transition name="slide-fade">
+      <keep-alive>
         <router-view></router-view>
-     </transition>
+      </keep-alive>
+
+    </transition>
     <!-- <footer-bar class="footer"></footer-bar> -->
   </div>
 </template>
 
 <script>
-  // import Footer from './components/FooterBar.vue'
-  export default {
-    name: 'app',
-    // components: {
-    //   'footer-bar': Footer
-    // },
-    computed: {}
-  }
+    // import Footer from './components/FooterBar.vue'
+    export default {
+        name: 'app',
+        // components: {
+        //   'footer-bar': Footer
+        // },
+        computed: {}
+    }
 </script>
 
 <style>
-@import '../static/font/iconfont.css';
-@import '../static/css/main.css';
-  body{
-    padding-bottom:1.56rem;
-    font-family: '微软雅黑';
-    background: #F4F4F4;;
-}
-/* 可以设置不同的进入和离开动画 */
-/* 设置持续时间和动画函数 */
-.slide-fade-enter-active {
-  transition: all .1s ease;
-}
-
-.picker-items{
-  display: flex !important;
-  align-items: center !important;
-  justify-content: center !important;
-}
+    @import '../static/font/iconfont.css';
+    @import '../static/css/main.css';
+    body {
+        /* padding-bottom: 1.56rem; */
+        font-family: '微软雅黑';
+        background: #F4F4F4;
+        ;
+    }
+    
+    .slide-fade-enter-active {
+        transition: all .3s ease;
+    }
+    
+    .slide-fade-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    
+    .slide-fade-enter,
+    .slide-fade-leave-to
+    /* .slide-fade-leave-active for below version 2.1.8 */
+    
+    {
+        transform: translateX(10px);
+        opacity: 0;
+    }
+    
+    .picker-items {
+        display: flex !important;
+        align-items: center !important;
+        justify-content: center !important;
+    }
 </style>
--- vue_mint_app/src/components/header.vue
+++ vue_mint_app/src/components/header.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <header>
-      <i class="iconfont icon-fanhui fz" @click="this.$router.go(-1)"></i> <span class="title">
-            {{name}}
-         </span>
+      <i class="iconfont icon-fanhui fz" @click="$router.go(-1)"></i>
+      <span class="title">
+        {{name}}
+      </span>
     </header>
     <!-- 此处是占位符 -->
     <div class="zanweifu"></div>
@@ -11,51 +12,54 @@
 </template>
 
 <script>
-  export default {
-    name: 'header',
-    data: function() {
-      return {
-        name:'',
-      }
-    },
-    created () {
-      this.getName();
-    },
-    methods: {
-      // 接收名字
-      getName:function(){
-        console.log(this.$route.query.name)
-        var name=this.$route.query.name;
-        this.name=name;
-      }
+    export default {
+        name: 'header',
+        data: function() {
+            return {
+                name: '',
+            }
+        },
+        created() {
+            this.getName();
+        },
+        methods: {
+            // 接收名字
+            getName: function() {
+                console.log(this.$route.query.name)
+                var name = this.$route.query.name;
+                this.name = name;
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-  header {
-    height: 0.88rem;
-    background: #f5f5f5;
-    font-size: 0.32rem;
-    color: #353535;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    z-index: 99;
-  }
-  .fz {
-    font-size: 0.48rem;
-    color: #353535;
-  }
-  .title {
-    display: inline-block;
-    padding-left: 0.2rem;
-  }
-  .zanweifu {
-    margin-top: 0.88rem;
-  }
+    header {
+        height: 0.88rem;
+        background: #f5f5f5;
+        font-size: 0.32rem;
+        color: #353535;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        z-index: 99;
+    }
+    
+    .fz {
+        font-size: 0.48rem;
+        color: #353535;
+    }
+    
+    .title {
+        display: inline-block;
+        padding-left: 0.2rem;
+    }
+    
+    .zanweifu {
+        margin-top: 0.88rem;
+    }
 </style>
