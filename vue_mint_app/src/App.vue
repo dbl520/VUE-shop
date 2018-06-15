6d94cbc85db7482ff30efd356684d5c963cb311d
zhuzhudengbaoling <2239146847@qq.com>
2018-06-15 14:54:48 +0800

    1
    1

--- vue_mint_app/src/App.vue
+++ vue_mint_app/src/App.vue
@@ -30,10 +30,9 @@
         background: #F4F4F4;
         ;
     }
-    
-    .slide-fade-enter-active {
+    /* .slide-fade-enter-active {
         transition: all .3s ease;
-    }
+    } */
     
     .slide-fade-leave-active {
         transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
