52d28c57cb7401450c92d4951c7c5af732d73721
zhuzhudengbaoling <2239146847@qq.com>
2018-06-19 23:33:16 +0800

    1
    1

--- vue_mint_app/src/pages/choose.vue
+++ vue_mint_app/src/pages/choose.vue
@@ -43,27 +43,15 @@
 
       <mt-tab-container-item id="tab2" class="tab_phone">
 
-        <div class="xli">
+        <div class="xli" v-for="(phone,index) in all.tab2"  @click="xuangou(phone.id)" v-bind:id="phone.id">
 
           <div class="xl_img">
 
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
+            <img v-bind:src="phone.ImageOne">
 
           </div>
 
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
-
-        </div>
-
-        <div class="xli">
-
-          <div class="xl_img">
-
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
-
-          </div>
-
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
+          <p class="xl_p">{{phone.name}}</p>
 
         </div>
 
@@ -71,58 +59,34 @@
 
       <mt-tab-container-item id="tab3" class="tab_phone">
 
-        <div class="xli">
+        <div class="xli" v-for="(phone,index) in all.tab3"  @click="xuangou(phone.id)" v-bind:id="phone.id">
 
           <div class="xl_img">
 
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
+            <img v-bind:src="phone.ImageOne">
 
           </div>
 
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
+          <p class="xl_p">{{phone.name}}</p>
 
         </div>
 
-        <div class="xli">
-
-          <div class="xl_img">
-
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
-
-          </div>
-
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
-
-        </div>
 
       </mt-tab-container-item>
 
-      <mt-tab-container-item id="tab4" class="tab_phone">
+      <mt-tab-container-item id="tab4" class="tab_phone" >
 
-        <div class="xli">
-
-          <div class="xl_img">
-
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
-
-          </div>
-
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
-
-        </div>
-
-        <div class="xli">
+        <div class="xli"  v-for="(phone,index) in all.tab4"  @click="xuangou(phone.id)" v-bind:id="phone.id">
 
           <div class="xl_img">
 
-            <img src="http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg">
+            <img v-bind:src="phone.ImageOne">
 
           </div>
 
-          <p class="xl_p">vivo x11手机 vivo x11手机</p>
+          <p class="xl_p">{{phone.name}}</p>
 
         </div>
-
       </mt-tab-container-item>
 
     </mt-tab-container>
@@ -130,128 +94,111 @@
   </div>
 </template>
 <script>
-// 引入共用头部组件
+    // 引入共用头部组件
 
-import header from "../components/header.vue";
+    import header from "../components/header.vue";
 
-export default {
-  // 初始化
+    export default {
+        // 初始化
 
-  components: {
-    "v-header": header
-  },
-
-  data() {
-    return {
-      selected: "tab1",
-
-      url:
-        "https://www.easy-mock.com/mock/59e95287dd7e1a0a448c1102/example/choose",
-
-      lists: []
-    };
-  },
-
-  mounted: function() {
-    // 一进来就执行 ,类型小程序onload
-
-    this.getData();
-  },
-
-  methods: {
-    getData: function() {
-      var _this = this;
-
-      _this.$http.get(_this.url).then(
-        response => {
-          console.log(response);
-
-          // get body data
-
-          _this.lists = response.data;
+        components: {
+            "v-header": header
         },
 
-        response => {
-          // error callback
+        data() {
+            return {
+                selected: "tab1",
+
+                url: "https://www.easy-mock.com/mock/59e95287dd7e1a0a448c1102/example/choose",
+
+                lists: [],
+                all: [],
+            };
+        },
+
+        mounted: function() {
+            // 一进来就执行 ,类型小程序onload
+
+            this.getData();
+        },
+
+        methods: {
+            getData: function() {
+                var _this = this;
+
+                _this.$http.get(_this.url).then(
+                    response => {
+                        console.log(response, '666');
+
+                        // get body data
+                        _this.all = response.data;
+                        _this.lists = response.data.tab1;
+                    },
+
+                    response => {
+                        // error callback
+                    }
+                );
+            },
+
+            xuangou: function(id) {
+                var _this = this;
+
+                _this.$router.push({
+                    path: "details",
+                    query: {
+                        id: id
+                    }
+                });
+            }
         }
-      );
-    },
-
-    xuangou: function(id) {
-      var _this = this;
-
-      _this.$router.push({
-        path: "details",
-
-        query: {
-          id: id
-        }
-      });
-    }
-  }
-};
+    };
 </script>
 <style scoped>
-.tab_phone {
-  display: flex;
-
-  align-items: center;
-
-  justify-content: space-between;
-
-  flex-wrap: wrap;
-
-  padding: 0 0.2rem;
-}
-
-.xl_img {
-  height: 3rem;
-}
-
-.xl_img img {
-  width: 100%;
-
-  height: 100%;
-}
-
-.xli {
-  width: 3.4rem;
-
-  margin-top: 0.2rem;
-
-  background: #fff;
-}
-
-.xl_p {
-  color: #353535;
-
-  text-align: center;
-
-  overflow: hidden;
-
-  text-overflow: ellipsis;
-
-  display: -webkit-box;
-
-  -webkit-line-clamp: 2;
-
-  -webkit-box-orient: vertical;
-}
-
-.mint-tab-container-item {
-  width: 95%;
-}
-
-.prices {
-  color: #fe498f;
-
-  font-size: 0.1rem;
-
-  text-align: center;
-}
-
-.price {
-  font-size: 0.4rem;
-}
+    .tab_phone {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        padding: 0 0.2rem;
+    }
+    
+    .xl_img {
+        height: 3rem;
+    }
+    
+    .xl_img img {
+        width: 100%;
+        height: 100%;
+    }
+    
+    .xli {
+        width: 3.4rem;
+        margin-top: 0.2rem;
+        background: #fff;
+    }
+    
+    .xl_p {
+        color: #353535;
+        text-align: center;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+    
+    .mint-tab-container-item {
+        width: 95%;
+    }
+    
+    .prices {
+        color: #fe498f;
+        font-size: 0.1rem;
+        text-align: center;
+    }
+    
+    .price {
+        font-size: 0.4rem;
+    }
 </style>
-
