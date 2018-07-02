1470ce35fd5d4550e68ae79282c51d5a9a7b0da6
zhuzhudengbaoling <2239146847@qq.com>
2018-07-02 12:24:51 +0800

    9
    9

--- vue_mint_app/src/components/FooterBar.vue
+++ vue_mint_app/src/components/FooterBar.vue
@@ -1,45 +1,45 @@
 <template>
   <div class="bottom">
-    <div class="bottom-ul"> 
-      <router-link  to="/main" tag="div">
+    <div class="bottom-ul">
+      <router-link to="/main" tag="div" style="width:20%">
         <div class="index">
           <img src="../../static/images/tab_1.png" v-if="active =='main'" class="img">
           <!-- <i class="iconfont icon-shouye_xuanzhong" style="color:#179dfe" v-if="active ==='main'"></i> -->
           <!-- <i class="iconfont icon-shouye2" v-else></i> -->
-          <img src="../../static/images/tab1.png" v-else class="img"/>
+          <img src="../../static/images/tab1.png" v-else class="img" />
         </div>
         <span class="name"> 首页</span>
       </router-link>
 
-      <router-link to="/fenlei" tag="div">
+      <router-link to="/fenlei" tag="div" style="width:20%">
         <div class="index">
-            <img src="../../static/images/tab_2.png" v-if="active =='fenlei'" class="img" />
-            <img src="../../static/images/tab.png" v-else class="img" />
+          <img src="../../static/images/tab_2.png" v-if="active =='fenlei'" class="img" />
+          <img src="../../static/images/tab.png" v-else class="img" />
         </div>
         <span class="name">分类</span>
       </router-link>
 
-      <router-link to="/tool" tag="div">
+      <router-link to="/tool" tag="div" style="width:20%">
         <div class="index">
-            <img src="../../static/images/tab5_.png" v-if="active =='tool'" class="img" />
-            <img src="../../static/images/tab5.png" v-else  class="img" />
+          <img src="../../static/images/tab5_.png" v-if="active =='tool'" class="img" />
+          <img src="../../static/images/tab5.png" v-else class="img" />
         </div>
         <span class="name">新闻</span>
       </router-link>
-      <router-link to="/cart" tag="div">
+      <router-link to="/cart" tag="div" style="width:20%">
         <div class="index">
           <!-- <i class="iconfont icon-dongtaixuanzhong" style="color:#179dfe" v-if="active ==='car'"></i>
           <i class="iconfont icon-dongtai" v-else></i> -->
-          <img src="../../static/images/tab3_.png" v-if="active =='cart'"  class="img"/>
-          <img src="../../static/images/tab3.png"  v-else  class="img" />
+          <img src="../../static/images/tab3_.png" v-if="active =='cart'" class="img" />
+          <img src="../../static/images/tab3.png" v-else class="img" />
         </div>
         <span class="name">购物车</span>
       </router-link>
 
-       <router-link to="/my" tag="div">
+      <router-link to="/my" tag="div" style="width:20%">
         <div class="index">
-            <img src="../../static/images/tab4_.png" v-if="active =='my'" class="img" />
-            <img src="../../static/images/tab4.png" v-else  class="img" />
+          <img src="../../static/images/tab4_.png" v-if="active =='my'" class="img" />
+          <img src="../../static/images/tab4.png" v-else class="img" />
         </div>
         <span class="name">我的</span>
       </router-link>
@@ -70,35 +70,38 @@
         background: #fff;
         font-size: 0.34rem;
     }
-    
+
     .index {
         text-align: center;
+        width:20%;
     }
-    
+
     .bottom-ul {
         display: flex;
         align-items: center;
         justify-content: space-between;
         background: #fff;
-        padding: 0.1rem 0.4rem;
+        padding: 0.1rem 0;
+        width: 100%;
+        height:0.98rem;
     }
-    
+
     .bottom-ul div {
         display: flex;
         align-items: center;
         flex-direction: column;
         list-style: none;
     }
-    
+
     .name {
         display: inline-block;
     }
-    
+
     .img {
         width: .5rem;
         height: .5rem;
     }
-    
+
     .router-link-active {
         color: red;
     }
@@ -117,7 +120,7 @@
         justify-content: center;
         flex-direction: column;
         text-align center
-        i 
+        i
           font-size .55rem
       .router-link:hover,.router-link-active
         color: #25b5fe; */
